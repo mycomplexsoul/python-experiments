8f38c86b099e0ccee5eec3fd5ab8776ca7c4b1e4
fernando <link903@hotmail.com>
2022-10-19 19:13:43 -0500

    Se agrego un sistema de apuestas

--- src/volados.py
+++ src/volados.py
@@ -1,48 +1,72 @@
 import random
 
-print ("""          --- Juguemos valados --- 
+print ("""          --- Jugemos valados --- 
 
 * La computadora lanzara una moneda y tu objetibo es adivinar si cayo (cara) o (cruz)
 
-* Comenzaras con 5 monedas, ganaras una extra por cada vez que aciertes y perderas una si fallas
+* Puedes comenzaras con un maximo de 50 monedas
 
-* El juego acaba si te quedas sin monedas o consiges 10 monedas
+* El juego acaba si te quedas sin monedas o consiges 100 monedas
 
 * Al final de cada intento se te mostrara que eligio la computadora 
 
 * Si en cualquer momento quieres retirarte. Escribe (salir) y el juego terminara
 """)
 
+contador = int(input ("Cuantas monedas tienes: "))
+if contador > 50:
+    contador = int (input("El numero de monedas no debe ser mayor a 50: "))
+    while contador > 50 or contador < 0:
+        if contador > 50:
+            contador = int(input("Ese numero es muy grande. Pueba de nuevo: "))
+        elif contador <= 0:
+            contador = 5 
+            print("Ya que no tienes monedas te daremos 5: ")
+if contador <= 0:
+    contador = 5 
+    (print("Ya que no tienes monedas te daremos 5: "))
+
 lanzamiento = ("Elige cara o cruz: ")
 eleccion = input (lanzamiento)
-contador = 5
 
-while contador != 0:
-    
+
+while contador != 0:# compararlo con la cantidad actual
+   
+    if eleccion == "salir":
+        exit ("Te quedaron "+ str(contador)+ " monedas. Estuviste cerca gracias por jugar")    
     # lanzamiento_computadora = random.randint(1,2)
     
     # if lanzamiento_computadora == 1:
     #     lanzamiento_computadora = "cara"
     # if lanzamiento_computadora == 2 :
     #     lanzamiento_computadora = "cruz"
+        #agregar la opcion de que cantidad quieren apostar
+    apuesta =int(input("Cuanto quieres apostar: "))  
+    while apuesta >= 0:
 
+        if apuesta >  contador:
+            apuesta =  int(input("no puedes apostar mas de lo que tienes: "))
+
+        if apuesta <= contador:
+            print ("La apuesta actual es de: " +str(apuesta))
+            apuesta = int(apuesta)
+            break
+ 
+    
     lanzamiento_computadora =("cara", "cruz")
     lanzamiento_computadora = random.choice(lanzamiento_computadora)
-    
-    if eleccion == "salir":
-        exit ("Te quedaron "+ str(contador)+ " monedas. Estuviste cerca gracias por jugar")
-
+ 
     if eleccion == lanzamiento_computadora:
-        contador += 1
-        if contador == 10:
-            exit ("Felizidades llegaste a 10 monedas ")
+        contador = int(contador)+(apuesta)
+        if contador == 100:
+            exit ("Felizidades llegaste a 100 monedas.")
         else:
             print ("Ganaste, tienes: " + str(contador)+" monedas.")
 
     elif eleccion != lanzamiento_computadora:
-        contador -= 1
-        if contador == 0:
-           exit("Ya no te quedan monedas")
+        contador = int(contador)-(apuesta)
+        if contador <= 0:
+           exit("Perdiste, ya no te quedan monedas")
         else:
             print ("Perdiste, tienes: "+ str(contador)+" monedas.")
 
